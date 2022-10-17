8394a36221df3dadb110d98b09097e4f67515ad7
jquintanillac <juanqcalderon@gmail.com>
2022-10-17 15:51:28 -0500

    Cambio de la interfaz Web a c#

---
+++ Deportivo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Deportivo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
